87871cbf409fe38b1fde7d5beeaedbd50b654a07
Shivashankar Raje Urs <shivashankarrajeurs@gmail.com>
2025-03-15 15:29:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import dotenv from 'dotenv';
 dotenv.config();
 
-const port = 3000;
+const port =process.env.PORT||3000;
 const app = express();
 
 const apikey = process.env.API_KEY; // API Key
